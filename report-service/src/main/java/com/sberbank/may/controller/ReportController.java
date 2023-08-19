1b9f8cb1f033c4aa5c4856db68387bcf099ad1be
Никитин Егор Игоревич <EINikitin@sberbank.ru>
2023-08-19 17:54:51 +0300

    add read pdf file egor

---
+++ report-service/src/main/java/com/sberbank/may/controller/ReportController.java
@@ -0,0 +1,27 @@
+package com.sberbank.may.controller;
+
+
+import com.sberbank.may.dto.ReportData;
+import com.sberbank.may.service.GenerateAvg;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class ReportController {
+
+    @PostMapping(value = "/report-avg", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> generateReport(@RequestBody ReportData reportData) {
+        GenerateAvg generateAvg = new GenerateAvg();
+       return generateAvg.generatePdfAvg(reportData);
+    }
+
+
+
+
+}
