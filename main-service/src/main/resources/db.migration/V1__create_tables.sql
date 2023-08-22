16bc2d8041117b414bbeb13f69f2c682d30b40ac
Никитин Егор Игоревич <EINikitin@sberbank.ru>
2023-08-22 21:53:03 +0300

    добавлен flyway

---
+++ main-service/src/main/resources/db.migration/V1__create_tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE public.flyway_schema_history
+(
+    installed_rank INT                     NOT NULL,
+    version        VARCHAR(50),
+    description    VARCHAR(200)            NOT NULL,
+    type           VARCHAR(20)             NOT NULL,
+    script         VARCHAR(1000)           NOT NULL,
+    checksum       INT,
+    installed_by   VARCHAR(100)            NOT NULL,
+    installed_on   TIMESTAMP DEFAULT NOW() NOT NULL,
+    execution_time INT                     NOT NULL,
+    success        BOOLEAN                 NOT NULL
+);
