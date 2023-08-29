0c6cdc6e2c636f2be4cbb6a24cfbdbc1515dfff9
Vyacheslav <torlant312@yandex.ru>
2023-08-29 22:25:04 +0300

    Исправлен интеграционный тест lesson

--- main-service/src/test/java/com/sberbank/may/lesson/service/LessonServiceImplIntegrationTest.java
+++ main-service/src/test/java/com/sberbank/may/lesson/service/LessonServiceImplIntegrationTest.java
@@ -23,6 +23,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -52,13 +53,16 @@ class LessonServiceImplIntegrationTest {
     private User teacher1;
     private Predmet subject1;
     private StudentClass studentClass1;
+    private StudentClass studentClass2;
     private Lesson lesson1;
+    private Lesson lesson2;
     private Student student1;
 
     private User parent2;
     private User parent3;
 
     @BeforeEach
+    @Transactional
     void setUp() {
         teacher1 = new User(null, "Румянцева Анастасия Ивановна", "password",
                 "testMail@mail.ru", "+79211554785",
@@ -81,7 +85,7 @@ class LessonServiceImplIntegrationTest {
         subject2.setTitle("Русский язык");
         studentClass1 = new StudentClass();
         studentClass1.setTitle("5А");
-        StudentClass studentClass2 = new StudentClass();
+        studentClass2 = new StudentClass();
         studentClass2.setTitle("11Б");
         student1 = new Student();
         student1.setName("Расторгуев Максим Павлович");
@@ -99,7 +103,7 @@ class LessonServiceImplIntegrationTest {
         lesson1.setTeacher(teacher1);
         lesson1.setPredmet(subject1);
         lesson1.setStudentClass(studentClass1);
-        Lesson lesson2 = new Lesson();
+        lesson2 = new Lesson();
         lesson2.setStudentClass(studentClass2);
         lesson2.setLessonTime(LocalDateTime.now().plusDays(1));
         lesson2.setTeacher(teacher2);
@@ -109,71 +113,61 @@ class LessonServiceImplIntegrationTest {
         mark1.setLesson(lesson1);
         mark1.setStudent(student1);
         mark1.setValue(5);
+        userRepository.saveAndFlush(teacher1);
+        userRepository.saveAndFlush(teacher2);
+        predmetRepository.saveAndFlush(subject1);
+        predmetRepository.saveAndFlush(subject2);
+        studentClassRepository.saveAndFlush(studentClass1);
+        studentClassRepository.saveAndFlush(studentClass2);
+        userRepository.saveAndFlush(parent2);
+        userRepository.saveAndFlush(parent3);
+        userRepository.saveAndFlush(parent1);
+        studentRepository.saveAndFlush(student2);
+        studentRepository.saveAndFlush(student1);
+        lessonService.createLesson(lesson1);
     }
 
+    @SneakyThrows
     @DisplayName("Проверка создания урока")
     @Test()
     void createLesson() {
-        userRepository.save(teacher1);
-        predmetRepository.save(subject1);
-        studentClassRepository.save(studentClass1);
-        studentRepository.save(student1);
-        lessonService.createLesson(lesson1);
-        lessonRepository.findById(1L).ifPresent(lessonFromDbB -> assertThat(lesson1, is(lessonFromDbB)));
+        lessonService.createLesson(lesson2);
+        lessonRepository.findById(2L).ifPresent(lessonFromDbB -> assertThat(lesson2, is(lessonFromDbB)));
     }
 
+    @SneakyThrows
     @DisplayName("Проверка поиска уроков")
     @Test
     void searchLessons() {
-        userRepository.save(teacher1);
-        predmetRepository.save(subject1);
-        studentClassRepository.save(studentClass1);
-        studentRepository.save(student1);
-        lessonService.createLesson(lesson1);
         List<Lesson> lessons = lessonService.searchLessons("Румянцева Анастасия Ивановна", "5А",
                 "Математика",
-                lesson1.getLessonTime());
+                lesson1.getLessonTime().plusMinutes(10));
         assertThat(lessons, contains(lesson1));
     }
 
+    @SneakyThrows
     @DisplayName("Проверка удаления урока")
     @Test
     void deleteById() {
-        userRepository.save(teacher1);
-        userRepository.save(parent2);
-        userRepository.save(parent3);
-        predmetRepository.save(subject1);
-        studentClassRepository.save(studentClass1);
-        studentRepository.save(student1);
-        lessonService.createLesson(lesson1);
         assertThat(lessonRepository.findAll(), contains(lesson1));
         lessonService.deleteById(1L);
+        lessonService.deleteById(2L);
         List<Lesson> all = lessonRepository.findAll();
         assertThat(all, empty());
     }
 
+    @SneakyThrows
     @DisplayName("Проверка поиска урока по id")
     @Test
     void findLessonById() {
-        userRepository.save(teacher1);
-        userRepository.save(parent2);
-        userRepository.save(parent3);
-        predmetRepository.save(subject1);
-        studentClassRepository.save(studentClass1);
-        studentRepository.save(student1);
         lessonService.createLesson(lesson1);
         assertThat(lessonService.findLessonById(1L), is(lesson1));
     }
 
+    @SneakyThrows
     @DisplayName("Проверка обновления урока")
     @Test
     void updateLesson() {
-        userRepository.save(teacher1);
-        userRepository.save(parent2);
-        userRepository.save(parent3);
-        predmetRepository.save(subject1);
-        studentClassRepository.save(studentClass1);
-        studentRepository.save(student1);
         lessonService.createLesson(lesson1);
         Lesson foundLesson = lessonRepository.findById(1L).orElseThrow();
         Homework newHomework = new Homework();
@@ -184,49 +178,30 @@ class LessonServiceImplIntegrationTest {
                 equalTo(newHomework.getDescription()));
     }
 
+    @SneakyThrows
     @DisplayName("Проверка получения всех уроков")
     @Test
     void getAllLessons() {
-        userRepository.save(teacher1);
-        userRepository.save(parent2);
-        userRepository.save(parent3);
-        predmetRepository.save(subject1);
-        studentClassRepository.save(studentClass1);
-        studentRepository.save(student1);
         lessonService.createLesson(lesson1);
         assertThat(lessonService.getAllLessons(), contains(lesson1));
     }
 
+    @SneakyThrows
     @DisplayName("Проверка поиска уроков учителя")
     @Test
     void searchLessonsForTeacher() {
-        userRepository.save(teacher1);
-        userRepository.save(parent2);
-        userRepository.save(parent3);
-        predmetRepository.save(subject1);
-        studentClassRepository.save(studentClass1);
-        studentRepository.save(student1);
-        lessonService.createLesson(lesson1);
-
         List<Lesson> lessons = lessonService.searchLessonsForTeacher(teacher1.getName(), studentClass1.getTitle(),
                 subject1.getTitle(),
-                lesson1.getLessonTime(), lesson1.getLessonTime().plusDays(3));
+                lesson1.getLessonTime().minusDays(1), lesson1.getLessonTime().plusDays(3));
         assertThat(lessons, contains(lesson1));
     }
 
+    @SneakyThrows
     @DisplayName("Проверка перегруженного метода поисков уроков учителя")
     @Test
     void testSearchLessonsForTeacher() {
-        userRepository.save(teacher1);
-        userRepository.save(parent2);
-        userRepository.save(parent3);
-        predmetRepository.save(subject1);
-        studentClassRepository.save(studentClass1);
-        studentRepository.save(student1);
-        lessonService.createLesson(lesson1);
-
-        List<Lesson> lessons = lessonService.searchLessonsForTeacher(teacher1.getName(), lesson1.getLessonTime(),
-                lesson1.getLessonTime().plusDays(3));
-        assertThat(lessons, contains(lesson1));
+        List<Lesson> lessons = lessonService.searchLessonsForTeacher(teacher1.getName(),
+                lesson1.getLessonTime().minusDays(1), lesson1.getLessonTime().plusDays(3));
+        assertThat(lessons.get(0), is(lesson1));
     }
 }
