49641e27f15df31aa9f79cc75e9d302338e5313a
Vyacheslav <torlant312@yandex.ru>
2023-08-30 19:34:02 +0300

    добавлены тесты для MarkServiceImpl

--- main-service/src/main/java/com/sberbank/may/mark/service/MarkServiceImpl.java
+++ main-service/src/main/java/com/sberbank/may/mark/service/MarkServiceImpl.java
@@ -11,18 +11,32 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Сервисный класс для работы с оценками студентов. Использует репозиторий для работы с данными.
+ */
 @Service
 @RequiredArgsConstructor
 public class MarkServiceImpl implements MarkService {
 
     private final MarkRepository markRepository;
 
+    /**
+     * Сохраняет или обновляет оценку студента в базе данных.
+     *
+     * @param mark Оценка студента
+     */
     @Transactional
     @Override
     public void saveMark(Mark mark) {
         markRepository.save(mark);
     }
 
+    /**
+     * Возвращает список студентов с оценками по определенному уроку.
+     *
+     * @param id ID урока
+     * @return список студентов с оценками
+     */
     @Override
     public List<StudentWithMarkOut> searchStudentsMarksOnLesson(long id) {
         return markRepository.searchStudentsMarksOnLesson(id).stream()
@@ -34,17 +48,34 @@ public class MarkServiceImpl implements MarkService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Удаляет оценку студента из базы данных по id.
+     *
+     * @param id ID оценки студента
+     */
     @Transactional
     @Override
     public void deleteById(long id) {
         markRepository.deleteById(id);
     }
 
+    /**
+     * Находит и возвращает оценку студента по ее id.
+     *
+     * @param id Id оценки студента
+     * @return оценку студента, если она найдена
+     * @throws NotFoundException если оценка не найдена
+     */
     @Override
     public Mark findMarkById(long id) {
         return markRepository.findById(id).orElseThrow(() -> new NotFoundException("Оценка с id=" + id + " не найдена"));
     }
 
+    /**
+     * Обновляет значение оценки студента.
+     *
+     * @param mark новые данные оценки студента
+     */
     @Override
     public void patchMark(Mark mark) {
         Mark markForUpdate = findMarkById(mark.getId());
